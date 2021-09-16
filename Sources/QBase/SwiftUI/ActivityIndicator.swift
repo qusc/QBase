f3c5ac63e0dd89a4274ef4be62bb92225930d73b
Quirin Schweigert <quirin.schweigert@outlook.com>
2021-09-16 10:14:49 +0200

    Add ActivityIndicator and CoveringGradientView. Refactor folder structure.

---
+++ Sources/QBase/SwiftUI/ActivityIndicator.swift
@@ -0,0 +1,27 @@
+import Foundation
+import SwiftUI
+import UIKit
+
+@available(iOS 14.0, *)
+public struct ActivityIndicator: UIViewRepresentable {
+    let color: UIColor
+
+    init(color: UIColor = .white) {
+        self.color = color
+    }
+
+    public func makeUIView(context: UIViewRepresentableContext<ActivityIndicator>)
+    -> UIActivityIndicatorView {
+        let acticityIndicatorView = UIActivityIndicatorView(style: .medium)
+        acticityIndicatorView.startAnimating()
+        acticityIndicatorView.color = color
+        return acticityIndicatorView
+    }
+
+    public func updateUIView(
+        _ uiView: UIActivityIndicatorView,
+        context: UIViewRepresentableContext<ActivityIndicator>
+    ) {
+        uiView.color = color
+    }
+}
---
+++ Sources/QBase/SwiftUI/CoveringGradientView.swift
@@ -0,0 +1,77 @@
+//
+//  CoveringGradientView.swift
+//  Haystack
+//
+//  Created by Quirin Schweigert on 05.08.21.
+//
+
+import SwiftUI
+
+public struct CoveringGradientView<ContentView>: View where ContentView: View {
+    let contentView: () -> ContentView
+
+    let alignment: Alignment
+    let color: Color
+    let size: CGFloat
+
+    public var body: some View {
+        contentView()
+            .overlay(
+                LinearGradient(
+                    gradient: Gradient(
+                        colors: [color, color.opacity(0)]
+                    ),
+                    startPoint: unitPoint(for: inverseAlignment(of: alignment)),
+                    endPoint: unitPoint(for: alignment)
+                )
+                .frame(
+                    width: alignment == .leading || alignment == .trailing ? size : nil,
+                    height: alignment == .top || alignment == .bottom ? size : nil
+                )
+                .allowsHitTesting(false),
+                alignment: alignment
+            )
+    }
+
+    func inverseAlignment(of alignment: Alignment) -> Alignment {
+        switch alignment {
+        case .top:
+            return .bottom
+        case .bottom:
+            return .top
+        case .leading:
+            return .trailing
+        case .trailing:
+            return .leading
+        default:
+            return alignment
+        }
+    }
+
+    func unitPoint(for alignment: Alignment) -> UnitPoint {
+        switch alignment {
+        case .top:
+            return .bottom
+        case .bottom:
+            return .top
+        case .leading:
+            return .trailing
+        case .trailing:
+            return .leading
+        default:
+            return .center
+        }
+    }
+}
+
+public extension View {
+    func coveringGradient(alignment: Alignment = .top, color: Color = .white, size: CGFloat = 20)
+    -> some View {
+        CoveringGradientView(
+            contentView: { self },
+            alignment: alignment,
+            color: color,
+            size: size
+        )
+    }
+}
